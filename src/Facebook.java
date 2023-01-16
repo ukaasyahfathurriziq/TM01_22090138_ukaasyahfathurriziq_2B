f478abbc5f02d43ff37c45c5c8a2b1948fcd78fc
Ukaasyahfathurriziq <fathurriziq55@gmail.com>
2023-01-17 05:43:55 +0700

    jwb saya

---
+++ src/Facebook.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Facebook {
+    private String namaPengguna;
+    private String password;
+
+    public void setNamaPengguna(String nmpengguna) {
+        this.namaPengguna = nmpengguna;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNamaPengguna() {
+        return this.namaPengguna;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
